91a0bb1ea0b1f44dddb6406a52c58190e7a762b6
Johnny Li <130239183+j0hnnyli@users.noreply.github.com>
2025-03-24 03:05:36 -0400

    developed a navbar for mobile screens

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,19 +1,14 @@
 import { FaInstagram, FaXTwitter } from "react-icons/fa6";
 import { FaFacebookF } from "react-icons/fa";
-
 import { IoIosCart } from "react-icons/io";
 import Link from "next/link";
 import NavbarWrapper from "./NavbarWrapper";
-
-const links = [
-  { name : 'Shop', href : '/shop/all'}, 
-  { name :'About', href: '/about'}, 
-  {name : 'Contact', href: '/contact'}
-]
+import navLinks from "@/lib/content/navLink";
+import NavLink from "./NavLink";
 
 export default function Navbar(){
   return (
-    <NavbarWrapper className="flex items-center justify-center bg-[var(--primary-color)] h-28 fixed top-0 w-full z-50">
+    <NavbarWrapper className="hidden md:flex items-center justify-center bg-[var(--primary-color)] h-28 fixed top-0 w-full z-50">
       <nav className="w-full max_width flex justify-between items-center py-2 px-5">
         <div className="flex items-center gap-2 text-lg w-full">
           <FaInstagram className="cursor-pointer hover:scale-105"/>
@@ -24,12 +19,12 @@ export default function Navbar(){
         <div className="flex flex-col justify-center items-center w-full">
           <Link href="/" className="text-2xl font_crimson tracking-widest cursor-pointer">Ascend</Link>
           <div className="flex mt-5 gap-3"> 
-            {links.map((link) => (
-              <div key={link.name} className="flex text-sm">
-                <div>
-                  <Link href={link.href} className="hover:mb-5">{link.name}</Link>
-                </div>
-              </div>
+            {navLinks.map(({href, name}) => (
+              <NavLink 
+                key={name}
+                href={href}
+                name={name}
+              />
             ))}
           </div>
         </div>
---
+++ lib/content/navLink.ts
@@ -0,0 +1,7 @@
+const navLinks = [
+  { name : 'Shop', href : '/shop/all'}, 
+  { name :'About', href: '/about'}, 
+  {name : 'Contact', href: '/contact'}
+]
+
+export default navLinks;
