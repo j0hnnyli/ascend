111553500ba28387ca5ac4612acdc7926e71dbc5
Johnny Li <130239183+j0hnnyli@users.noreply.github.com>
2025-03-23 21:45:23 -0400

    developed a shipping & return page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,7 @@
 import { FaInstagram, FaXTwitter } from "react-icons/fa6";
 import { FaFacebookF } from "react-icons/fa";
 import RevealOpacity from "@/components/animation-components/RevealOpacity";
-import ReavelY from "@/components/animation-components/RevealY";
+import RevealY from "@/components/animation-components/RevealY";
 
 export default function ContactPage(){
   return (
@@ -21,28 +21,28 @@ export default function ContactPage(){
           className="font_crimson text-xl"
         /> 
         
-        <ReavelY delayTime={0.4} className="mt-5">
+        <RevealY delayTime={0.4} className="mt-5">
           <h4 className="font_crimson text-xl">Customer Service</h4>
           <p className="underline">customerservice@ascend.com</p>
-        </ReavelY>
+        </RevealY>
         
-        <ReavelY delayTime={0.6} className="mt-5">
+        <RevealY delayTime={0.6} className="mt-5">
           <h4 className="font_crimson text-xl">Wholesale Inquiries</h4>
           <p className="underline">wholesaleinquiries@ascend.com</p>
-        </ReavelY>
+        </RevealY>
        
-        <ReavelY delayTime={0.8} className="mt-5">
+        <RevealY delayTime={0.8} className="mt-5">
           <h4 className="font_crimson text-xl">Press Inquiries</h4>
           <p className="underline">pressinquiries@ascend.com</p>
-        </ReavelY>
+        </RevealY>
 
-        <ReavelY delayTime={1} className="my-10 font_crimson text-xl">Follow Us!</ReavelY>
+        <RevealY delayTime={1} className="my-10 font_crimson text-xl">Follow Us!</RevealY>
 
-        <ReavelY delayTime={1.2} className="flex gap-5">
+        <RevealY delayTime={1.2} className="flex gap-5">
           <FaInstagram className="text-lg cursor-pointer hover:text-[var(--third-color)]"/>
           <FaFacebookF className="text-lg cursor-pointer hover:text-[var(--third-color)]"/>
           <FaXTwitter className="text-lg cursor-pointer hover:text-[var(--third-color)]"/>
-        </ReavelY>
+        </RevealY>
       </div>
     </div>
   )
