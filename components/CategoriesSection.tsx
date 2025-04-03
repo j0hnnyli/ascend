0294faebff84166a39f5660125fdd3cf099c4881
Johnny Li <130239183+j0hnnyli@users.noreply.github.com>
2025-04-03 02:31:36 -0400

    Feat : Developed a New Animation For CategoryCard  (#14)
    * added reccommendation section on the single product page
    
    * added LoadingSkeleton
    
    * added a new animation for categorycard component

--- components/CategoriesSection.tsx
+++ components/CategoriesSection.tsx
@@ -1,38 +1,33 @@
 'use client'
 
-import { useState } from 'react'
 import categoriesInfo from '@/lib/content/categoriesInfo';
 import { motion } from 'framer-motion';
-import CategoryExpandCard from './CategoryExpandCard';
+import CategoryCard from './CategoryCard';
 
 
 export default function CategoriesSection(){
-  const [openIndex, setOpenIndex] = useState<number | null>(null);
 
   return (
     <motion.div 
       variants={{
-        hidden: {},
+        hidden: {}, 
         show: {
           opacity: 1,
-          transition: { staggerChildren: 0.3, delayChildren: 0.1 }, 
         }
       }}
       initial="hidden"
       whileInView="show"
-      viewport={{once: false, amount: 0.25}}
-      className='flex flex-col lg:flex-row gap-5 justify-between'
+      viewport={{once: false, amount: 0.20}}
+      className='flex flex-col lg:flex-row gap-5'
     >
       {categoriesInfo.map((category, index) => (
-        <CategoryExpandCard
+        <CategoryCard
           key={category.id}
           index={index}
           href={category.href}
           title={category.title}
           img={category.img}
           description={category.description}
-          isOpen={openIndex === index}
-          setOpenIndex={() => setOpenIndex(openIndex === index ? null : index)}
         />
       ))}
     </motion.div>
