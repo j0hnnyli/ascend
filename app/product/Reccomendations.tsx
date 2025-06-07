36fcff9e2132a69ef5d46915d5bac214a5078d22
Johnny Li <130239183+j0hnnyli@users.noreply.github.com>
2025-06-07 18:39:19 -0400

    Update : Cleaned Up UI Layout (#27)
    * fixed bugs and changed categories section animations and UI layout
    
    * fixed layout for category cards
    
    * updated the about page content
    
    * cleaned up product page UI layout

--- app/product/Reccomendations.tsx
+++ app/product/Reccomendations.tsx
@@ -1,9 +1,6 @@
-import QuickAddButton from "@/components/QuickAddButton";
 import supabase from "@/lib/supabaseClient"
 import Product from "@/lib/types/productType";
-import Image from "next/image";
-import Link from "next/link";
-
+import ReccomendedDisplayContainer from "./ReccomendedDisplayContainer";
 
 type Props = {
   id: number;
@@ -18,48 +15,6 @@ export default async function Recommendations({ category, id } : Props){
   const filter = data.filter((product) => product.id !== id)
 
   return (
-    <div>
-      <div className="flex items-center gap-10 mt-20">
-        <Image 
-          src="/logo.png"
-          alt="logo"
-          width={50}
-          height={50}
-          className=""
-        />
-
-        <h2 className="font_crimson tracking-widest text-2xl md:text-4xl">
-          Recommendations 
-        </h2>
-      </div>
-
-      <div className="w-full flex gap-5 overflow-auto mt-5">
-        {filter.map((product) => (
-          <div 
-            key={product.id}
-            className="relative flex-shrink-0 w-[250px]" 
-          >
-            <Link href={`/product/${product.id}`} className="absolute h-full w-full z-20"></Link>
-            <div 
-              className="h-[400px] relative"
-            >
-              <Image
-                src={product.images[0]}
-                alt={product.title}
-                width={300}
-                height={400}
-                className="w-full h-full object-cover brightness-90"
-              />
-              <QuickAddButton product={product}/>
-            </div>
-      
-            <div className="text-center w-full">
-              <h2>{product.title}</h2>
-              <p>${product.price}</p>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
+      <ReccomendedDisplayContainer reccomendeds={filter}/>
   )
 }
