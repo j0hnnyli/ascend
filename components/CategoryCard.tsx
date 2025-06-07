4b7d50b5b3d3129ff79cb8bce8f947b8d37723dd
Johnny Li <130239183+j0hnnyli@users.noreply.github.com>
2025-06-07 14:40:36 -0400

    fixed layout for category cards

--- components/CategoryCard.tsx
+++ components/CategoryCard.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import Image from "next/image";
 import Link from "next/link";
 import { MdOutlineArrowOutward } from "react-icons/md";
@@ -17,14 +19,14 @@ export default function CategoryCard({ title, img, href }: Props) {
        <Link href={href}
        > 
         <div 
-          className="group bg-[var(--primary-color)] rounded-xl w-full relative  overflow-hidden h-[150px]"
+          className="group bg-[var(--primary-color)] rounded-xl w-full relative  overflow-hidden h-[150px] md:h-[200px]  lg:h-[150px]"
         >
-          <div className='flex items-center text-4xl p-1'>
+          <div className='flex items-center text-3xl lg:text-4xl p-1 bg-[var(--primary-color)]'>
             <h2 className=''>{title}</h2>
             <MdOutlineArrowOutward className='group-hover:ml-2 group-hover:mb-2 transition-all duration-300 ease-in-out text-xl mt-2'/>
           </div>
           <div 
-            className='absolute w-[200px] h-[200px] bottom-0 md:-bottom-1/2 -right-10 md:-right-10  lg:-right-20 group-hover:-bottom-1/4 group-hover:-right-2  transition-all duration-300 ease-in-out'
+            className='absolute w-[200px] h-[200px] -bottom-1/3 right-0 group-hover:right-5  transition-all duration-300 ease-in-out'
           >
             <Image 
               src={img}
